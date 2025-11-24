b34f93e23a914642ad7e0fe73b832e7750e2c1ff
Sandra Giurazzi <giurazzisandra@gmail.com>
2025-11-24 09:38:12 +0100

    Add scroll for mobile web home layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3848855780"
+    serviceWorkerVersion: "3909490258"
   }
 });
